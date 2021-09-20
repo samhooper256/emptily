0f8d3ca535de11e40c551c9e1122b9563aaf66c3
samho <samho@10.201.120.221>
2021-09-20 12:26:02 -0500

    Improved DoorGap documentation.

--- Graviton/src/rooms/DoorGap.java
+++ Graviton/src/rooms/DoorGap.java
@@ -8,10 +8,27 @@ public interface DoorGap {
 		return new DoorGapImpl(side, dist1, dist2);
 	}
 	
+	/** The {@link Side} of the {@link RoomLayout room} this {@link DoorGap} is on.*/
 	Side side();
-	
+
+	/**
+	 * <ul>
+	 * <li>If this gap's {@link #side() side} is {@link Side#TOP TOP} or {@link Side#BOTTOM BOTTOM}, returns the
+	 * distance from the left wall to the left side of this {@link DoorGap}.</li>
+	 * <li>If this gap's {@link #side() side} is {@link Side#LEFT LEFT} or {@link Side#RIGHT RIGHT}, returns the distance
+	 * from the top wall to the top side of this {@link DoorGap}.</li>
+	 * </ul>
+	 */
 	double dist1();
 	
+	/**
+	 * <ul>
+	 * <li>If this gap's {@link #side() side} is {@link Side#TOP TOP} or {@link Side#BOTTOM BOTTOM}, returns the
+	 * distance from the right wall to the right side of this {@link DoorGap}.</li>
+	 * <li>If this gap's {@link #side() side} is {@link Side#LEFT LEFT} or {@link Side#RIGHT RIGHT}, returns the
+	 * distance from the bottom wall to bottom side of this {@link DoorGap}.</li>
+	 * </ul>
+	 */
 	double dist2();
 	
 }
